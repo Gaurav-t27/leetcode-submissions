b7b6334128907b9b7d71cedb9fce515d5d8a40fd
Gaurav-t27 <182771245+Gaurav-t27@users.noreply.github.com>
2026-03-14 22:03:05 +0000

    LeetCode Synchronization - Longest Increasing Subsequence (C++)

--- problems/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ problems/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    vector<int> dp;
-    int dpUtil(const vector<int>& numG, int i)
-    {
-        if (dp[i] != -1)
-            return dp[i];
-
-        int maxLen = 1;
-        for (int j=i+1;j<numG.size();j++)
-        {
-            if(numG[i]<numG[j])
-                maxLen = max(maxLen, 1 + dpUtil(numG, j));
-        }
-
-        dp[i] = maxLen;
-        return maxLen;
-    }
     int lengthOfLIS(vector<int>& nums) {
-        dp.resize(nums.size(),-1);
-        int maxPath = 1;
+        vector<int> tail;
 
-        for (int i=0;i<nums.size();i++)
-        {
-            maxPath = max(maxPath, dpUtil(nums, i));
+        for(auto num:nums) {
+            auto it = std::lower_bound(tail.begin(),tail.end(),num);
+
+            if(it == tail.end()) {
+                tail.push_back(num);
+            } else {
+                *it = num;
+            }
         }
-
-        return maxPath;
+        return tail.size();
     }
 };
