261b70d0bae4097121c048c64e3ae7efffbea955
Gaurav-t27 <182771245+Gaurav-t27@users.noreply.github.com>
2026-05-18 19:02:41 +0000

    LeetCode Synchronization - Linked List Cycle (C++)

--- problems/0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ problems/0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,22 +9,13 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == nullptr)
-        {
-            return false;
-        }
-        if(head->next == nullptr)
-        {
-            return false;
-        }
-        ListNode* slow = head, *fast = head;
+        if(!head || !head->next) return false;
+        ListNode * slow = head, * fast = head;
 
-        while(fast != nullptr && fast->next!=nullptr)
-        {
-            slow = slow->next;
+        while(slow && fast && fast->next) {
             fast = fast->next->next;
-            if(slow == fast)
-                return true;
+            slow = slow->next;
+            if(fast == slow) return true;
         }
         return false;
     }
