70dc5a3b34f074830749e281982b0b9acba9990f
Gaurav-t27 <182771245+Gaurav-t27@users.noreply.github.com>
2026-05-19 20:44:46 +0000

    LeetCode Synchronization - Subtree of Another Tree (C++)

--- problems/0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ problems/0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,29 +11,20 @@
  */
 class Solution {
 public:
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (!subRoot) {
-            return true;
-        }
-        if (!root) {
-            return false;
-        }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q) return true;
+        if(!p || !q) return false;
 
-        if (sameTree(root, subRoot)) {
-            return true;
-        }
-        return isSubtree(root->left, subRoot) || 
-               isSubtree(root->right, subRoot);
+        if(p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root) return false;
 
-    bool sameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q) {
-            return true;
-        }
-        if (p && q && p->val == q->val) {
-            return sameTree(p->left, q->left) &&
-                   sameTree(p->right, q->right);
-        }
-        return false;
+        if(isSameTree(root, subRoot)) return true;
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
