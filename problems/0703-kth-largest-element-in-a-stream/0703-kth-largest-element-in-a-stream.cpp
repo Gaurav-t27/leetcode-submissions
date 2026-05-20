6024c0aafd93ac826cc541b7e022b33b9fb37932
Gaurav-t27 <182771245+Gaurav-t27@users.noreply.github.com>
2026-05-20 15:29:42 +0000

    LeetCode Synchronization - Kth Largest Element in a Stream (C++)

--- problems/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ problems/0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,25 +1,25 @@
 class KthLargest {
-    std::priority_queue<int, std::vector<int>, std::greater<int>> pq_;
+    priority_queue<int, vector<int>, greater<int>> pq;
     int k_;
 public:
-    KthLargest(int k, vector<int>& nums):k_(k) {
-        for(auto num:nums) {
-            add(num);
+    KthLargest(int k, vector<int>& nums): k_(k){
+        for(int num:nums) {
+            pq.push(num);
+            if(pq.size()>k) {
+                pq.pop();
+            }
         }
+        
     }
     
     int add(int val) {
-        if(pq_.size() < k_) {
-            pq_.push(val);
-        } else if(pq_.top() < val) {
-            pq_.pop();
-            pq_.push(val);
-        }
-        return pq_.top();
+        pq.push(val);
+        if(pq.size()>k_) pq.pop();
+        return pq.top();
     }
 };
 
-/**f
+/**
  * Your KthLargest object will be instantiated and called as such:
  * KthLargest* obj = new KthLargest(k, nums);
  * int param_1 = obj->add(val);
