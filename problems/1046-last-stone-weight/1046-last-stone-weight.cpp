5a9165e223a5c80f2f24fdc18cf1840f61ccd142
Gaurav-t27 <182771245+Gaurav-t27@users.noreply.github.com>
2026-05-20 18:22:17 +0000

    LeetCode Synchronization - Last Stone Weight (C++)

--- problems/1046-last-stone-weight/1046-last-stone-weight.cpp
+++ problems/1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq(stones.begin(),stones.end());
-        
-        while(pq.size()>1) {
-            int first = pq.top();
-            pq.pop();
-            int second = pq.top();
-            pq.pop();
-            if(first != second)
-                pq.push(first - second);
+        priority_queue<int> pq(stones.begin(), stones.end());
+
+        while(pq.size() > 1) {
+            int y = pq.top(); pq.pop();
+            int x = pq.top(); pq.pop();
+
+            pq.push(abs(y-x));
         }
-        return pq.empty() ? 0: pq.top();
+        return pq.top();
     }
 };
